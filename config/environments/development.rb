8eb81c30f9f3bdd21c04b02dad8178b10b72ff46
mason-fish <mason.e.fish@gmail.com>
2016-05-02 10:38:31 -0700

    remove docker and add threading to shell commands

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,9 +6,11 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #config.web_console.whitelisted_ips = '10.0.190.38'
+
   # Do not eager load code on boot.
   config.eager_load = false
-  
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
