70a2fa67d8c109bfe291c35c326865087677f44a
rickharris-dev <rickharris724@gmail.com>
2016-05-03 15:45:47 -0700

    Fixed show form error. Added to .gitignore.

--- rails-app/db/schema.rb
+++ rails-app/db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503204156) do
+ActiveRecord::Schema.define(version: 20160503004144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "audios", force: :cascade do |t|
     t.string   "text"
+    t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "url"
   end
 
   create_table "devices", force: :cascade do |t|
